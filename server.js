9f7638a2f37e0fd6e26d0522ad86a8ce10312cbd
Nenye <chinenyediei@gmail.com>
2019-07-25 20:54:12 -0500

    added basic html for forms

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
