b4b9c16e98dcec6b03dcf0ff677043c24df86548
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-07-31 08:15:39 -0500

    removing clutter

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -11,57 +11,7 @@ module.exports = function (app) {
   });
 }
 
-// var eventfulAPI = 'http://eventful.com/events?q=music';
 
-// $.ajax({
-//   get: eventfulAPI,
-//   method: 'GET'
-// }).then(function(xml) {
-//   console.log(xml)
-// })
-
-// *******************************************************
-// Changes XML to JSON
-// Use Function to Convert XML to JSON for 
-// Eventfull API Call.
-// *******************************************************
-function xmlToJson(xml) {
-	
-	// Create the return object
-	var obj = {};
-
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
 
   // Create a new example
 //   app.post("/api/examples", function(req, res) {
