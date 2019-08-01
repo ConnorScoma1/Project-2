4e546064fbddb6b06b27edb36148d164854da745
Nenye <chinenyediei@gmail.com>
2019-08-01 16:58:22 -0500

    API routing working correftly upon button pushes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,49 @@
 var Event = require("../models/event");
 
 module.exports = function (app) {
-    // Get all events
-    app.get("/api/all", function (req, res) {
-      Event.findAll({}).then(function (results) {
+  // Get all events
+  app.get("/api/all", function (req, res) {
+    Event.findAll({}).then(function (results) {
+      res.json(results);
+    });
+  });
+
+  //find specific event by name
+  app.get("/api/:eventName", function (req, res) {
+    if (req.params.book) {
+      Event.findAll({
+        where: {
+          eventName: req.params.eventName
+        }
+      }).then(function (results) {
         res.json(results);
       });
+    }
+  });
+
+  //find specific event by date
+  app.get("/api/:date", function (req, res) {
+    Event.findAll({
+      where: {
+        eventDate: req.params.eventDate
+      }
+    }).then(function (results) {
+      res.json(results);
     });
-  }
+  });
 
-//   debugger
-//   // Create a new example
-//   app.post("/api/new", function (req, res) {
-    
-//     Event.create({
-//       name: req.body.eventName,
-//       location: req.body.location,
-//       eventType: req.body.eventType,
-//       date: req.body.eventDate
 
-//     });
-//   });
 
-  
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     Event.destroy({ where: { id: req.params.id } }).then(function(
-//       results
-//     ) {
-//       res.json(results);
-//     });
+  //post routes//
+  //add book
+  app.post("/api/new", function (req, res) {
+    Event.create({
+      name: req.body.eventName,
+      location: req.body.location,
+      eventType: req.body.eventType,
+      date: req.body.eventDate
+    });
+  });
+}
+
 
